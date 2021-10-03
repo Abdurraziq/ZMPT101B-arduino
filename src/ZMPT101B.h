37307a02f92db8ec6e4898eba07115c9445298f3
Bulanov Konstantin <leech001@gmail.com>
2021-10-03 11:06:50 +0300

    Correct to use with ESP32

--- src/ZMPT101B.h
+++ src/ZMPT101B.h
@@ -3,21 +3,24 @@
 
 #include <Arduino.h>
 
-#define ADC_SCALE 1023.0
-#define VREF 5.0
+#define ADC_SCALE 4095.0
+#define VREF 3.3
 #define DEFAULT_FREQUENCY 50
 
-class ZMPT101B {
+class ZMPT101B
+{
 public:
 	ZMPT101B(uint8_t _pin);
 	int calibrate();
 	void setZeroPoint(int _zero);
 	void setSensitivity(float sens);
+	int getZeroPoint();
 	float getVoltageDC();
 	float getVoltageAC(uint16_t frequency = 50);
+	float calculateSensitivity(ZMPT101B sensor, float voltage);
 
 private:
-	int zero = 512;
+	int zero = 1800;
 	float sensitivity;
 	uint8_t pin;
 };
